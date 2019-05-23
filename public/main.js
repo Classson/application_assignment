8e038d28c8077a306c055c1b449e5d957e3557eb
Alex <Jillbk22@gmail.com>
2019-05-23 16:51:05 -0400

    installed random-movie-quotes and added a gif

--- public/main.js
+++ public/main.js
@@ -1,5 +1,10 @@
+
 fetch('/quote')
   .then(r => r.json())
   .then(data => {
     document.getElementById('quote').innerText = data.quote;
   });
+
+img = document.createElement('img');
+img.src = 'https://media.giphy.com/media/3o7rc0qU6m5hneMsuc/giphy.gif';
+document.getElementById('imgDiv').appendChild(img)
