629ef0aebd5d87ed8775f1e1a16e26737ded4bf6
Alex <Jillbk22@gmail.com>
2019-05-23 16:58:45 -0400

    added a few random gifs

--- public/main.js
+++ public/main.js
@@ -1,10 +1,25 @@
-
 fetch('/quote')
   .then(r => r.json())
   .then(data => {
     document.getElementById('quote').innerText = data.quote;
   });
 
+const gifs = [
+  'https://media.giphy.com/media/3o7rc0qU6m5hneMsuc/giphy.gif',
+  'https://media.giphy.com/media/VxbP9tLeKzazm/giphy.gif',
+  'https://media.giphy.com/media/vaRCdgM0fLNrW/giphy.gif',
+  'https://media.giphy.com/media/26DOMQa5Ib2SmaRZm/giphy.gif',
+  'https://media.giphy.com/media/3o7TKqB7Y98alpjTu8/giphy.gif'
+]
+
+function randomize (arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
 img = document.createElement('img');
-img.src = 'https://media.giphy.com/media/3o7rc0qU6m5hneMsuc/giphy.gif';
+img.src = randomize(gifs)
 document.getElementById('imgDiv').appendChild(img)
+
+
+
+
